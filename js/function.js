2a6dc105f8b815d0faf83e95d502e13da84e9b4f
mramoszwenger <84237961+mramoszwenger@users.noreply.github.com>
2023-02-22 17:26:17 -0300

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,17 @@
+document.getElementById('masInfo1').addEventListener('click', function() {
+    console.log("mostrar más información")
+
+    alert("TAREAS REALIZADAS: Planificación e Implementación de soluciones de networking. Configuración y puesta en marcha de redes. Definir y mantener políticas de backup y restore. Generación de documentación. Mantenimiento preventivo y correctivo en instalaciones de networking y de VoIP. Monitoreo de VPNs y enlaces de infraestructura. Trabajo en conjunto con el Administrador de Servidores para asegurar la funcionalidad de los sistemas. Soporte al cliente.")
+})
+
+document.getElementById('masInfo2').addEventListener('click', function() {
+    console.log("mostrar más información")
+
+    alert("TAREAS REALIZADAS: Responsable de la enseñanza y evaluación del área Informática en las distintas carreras de nivel Técnico/Terciario dictadas por la institución.")
+})
+
+document.getElementById('masInfo3').addEventListener('click', function() {
+    console.log("mostrar más información");
+    
+    alert("TAREAS REALIZADAS: Reclutamiento de personal de puesto fijo y temporario. Capacitación e incentivación del personal, a nivel individual y en grupo (trabajo en equipo). Coordinación del personal (asignación de tareas y horarios laborales de acuerdo a la necesidad del complejo). Personal a cargo, dependiendo entre temporada alta y baja, de 110 a 50 respectivamente. Realización de estrategias de captación de clientes y ventas. Logro de objetivos. Análisis de costo por producto y per cápita. Productividad en relación al attendance y horas trabajadas del personal. Resolución, atención de quejas y sugerencias. Reporte directo de resultados y acontecimientos, a la cúpula gerencial de la empresa. Utilización, sistema de gestión E-WAVE Microsoft.");
+})
